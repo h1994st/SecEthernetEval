72c44ab7fbbeff04c869cb4ec57c5f6e7476914f
h1994st <h1994st@gmail.com>
2020-11-26 13:56:07 -0500

    Add config.sh

---
+++ config.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+sudo cp /code/ipsec-strongswan-confs/alice/aliceRsaKey.pem /etc/ipsec.d/private
+sudo cp /code/ipsec-strongswan-confs/alice/aliceRsaCert.pem /etc/ipsec.d/certs
+sudo cp /code/ipsec-strongswan-confs/bob/bobRsaKey.pem /etc/ipsec.d/private
+sudo cp /code/ipsec-strongswan-confs/bob/bobRsaCert.pem /etc/ipsec.d/certs
+sudo cp /code/ipsec-strongswan-confs/caCert.pem /etc/ipsec.d/cacerts
+
+sudo cp /code/ipsec-strongswan-confs/alice/aliceEcc256Key.pem /etc/ipsec.d/private
+sudo cp /code/ipsec-strongswan-confs/alice/aliceEcc256Cert.pem /etc/ipsec.d/certs
+sudo cp /code/ipsec-strongswan-confs/bob/bobEcc256Key.pem /etc/ipsec.d/private
+sudo cp /code/ipsec-strongswan-confs/bob/bobEcc256Cert.pem /etc/ipsec.d/certs
+sudo cp /code/ipsec-strongswan-confs/caEcc256Cert.pem /etc/ipsec.d/cacerts
+
+if [ -z "$MY_NAME" ]
+then
+    sudo cp /code/ipsec-strongswan-confs/"$MY_NAME"/ipsec.conf /etc
+    sudo cp /code/ipsec-strongswan-confs/"$MY_NAME"/ipsec.secrets /etc
+    sudo cp /code/ipsec-strongswan-confs/"$MY_NAME"/strongswan.conf /etc
+fi
