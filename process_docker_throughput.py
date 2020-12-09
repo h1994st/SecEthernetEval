510baa718664a3ea4d2ab0311ff1c3fbbb75b20d
h1994st <h1994st@gmail.com>
2020-12-09 10:41:28 -0500

    Add new results

--- process_docker_throughput.py
+++ process_docker_throughput.py
@@ -54,9 +54,7 @@ def plot_throughput(throughput_avgs, throughput_stds):
     # plt.savefig('throughput_figure.png')
 
 
-def process_result_files(data_dir_path, topic):
-    assert os.path.exists(data_dir_path)
-
+def process_result_json_files(data_dir_path, topic):
     throughput_avgs = []
     throughput_stds = []
     for bit_rate in range(100, 1001, 100):
@@ -78,14 +76,56 @@ def process_result_files(data_dir_path, topic):
     return throughput_avgs, throughput_stds
 
 
+def process_result_text_files(data_dir_path, topic):
+    throughput_avgs = []
+    throughput_stds = []
+    for bit_rate in range(100, 1001, 100):
+        result_path_pattern = os.path.join(
+            data_dir_path, '%s_client_%dMbit_*.log' % (topic, bit_rate))
+        result_list = glob.glob(result_path_pattern)
+
+        throughputs = []
+        for result_path in result_list:
+            with open(result_path, 'r') as fp:
+                for line in fp:
+                    if line.find('sender') != -1:
+                        end_pos = line.find('bits/sec')
+                        start_pos = line.find('MBytes') + 6
+                        throughput = int(line[start_pos:end_pos])
+                        break
+            throughputs.append(throughput)
+
+        # avg & std dev
+        throughput_avgs.append(np.mean(throughputs) / 1000 / 1000)  # to Mbps
+        throughput_stds.append(np.std(throughputs) / 1000 / 1000)  # to Mbps
+
+    return throughput_avgs, throughput_stds
+
+
+def process_result_files(data_dir_path, topic, result_type):
+    assert os.path.exists(data_dir_path)
+    if result_type == 'json':
+        return process_result_json_files(data_dir_path, topic)
+    if result_type == 'text':
+        return process_result_text_files(data_dir_path, topic)
+    return 0, 0
+
+
 def main(data_dir):
     # Load meta.json
     meta_json_path = os.path.join(data_dir, 'meta.json')
     with open(meta_json_path, 'r') as fp:
         meta_json = json.load(fp)
-        ipsec_dir = meta_json.get('ipsec')
-        macsec_dir = meta_json.get('macsec')
-        none_dir = meta_json.get('none')
+        ipsec_entry = meta_json.get('ipsec')
+        macsec_entry = meta_json.get('macsec')
+        none_entry = meta_json.get('none')
+
+        ipsec_dir = ipsec_entry.get('dir')
+        ipsec_type = ipsec_entry.get('type')
+        macsec_dir = macsec_entry.get('dir')
+        macsec_type = macsec_entry.get('type')
+        none_dir = none_entry.get('dir')
+        none_type = none_entry.get('type')
 
     if ipsec_dir is None or macsec_dir is None:
         print('No data!')
@@ -94,18 +134,18 @@ def main(data_dir):
     # IPsec
     ipsec_dir_path = os.path.join(data_dir, ipsec_dir)
     ipsec_throughput_avgs, ipsec_throughput_stds = process_result_files(
-        ipsec_dir_path, 'ipsec_eval')
+        ipsec_dir_path, 'ipsec_eval', ipsec_type)
 
     # MACsec
     macsec_dir_path = os.path.join(data_dir, macsec_dir)
     macsec_throughput_avgs, macsec_throughput_stds = process_result_files(
-        macsec_dir_path, 'macsec_eval')
+        macsec_dir_path, 'macsec_eval', macsec_type)
 
     if none_dir is not None:
         # No security protocols
         none_dir_path = os.path.join(data_dir, none_dir)
         none_throughput_avgs, none_throughput_stds = process_result_files(
-            none_dir_path, 'none_eval')
+            none_dir_path, 'none_eval', none_type)
         print('Baseline:')
         print(none_throughput_avgs)
         print(none_throughput_stds)
