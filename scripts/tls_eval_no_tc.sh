7bafc12c00ad9c4d5014f97465ec19bb14efe23f
h1994st <h1994st@gmail.com>
2021-01-13 22:06:18 -0500

    Update TLS evaluation scripts

--- scripts/tls_eval_no_tc.sh
+++ scripts/tls_eval_no_tc.sh
@@ -10,7 +10,7 @@ docker-compose up -d
 # Run iperf3
 docker-compose exec -T alice /code/scripts/iperf3_wolfssl_start_server.sh \
     $TOPIC "tls_all"
-DATA_SIZE=104857600  # 100 MB of data
+TEST_DURATION=120  # 2 mins
 SERVER_IP_ADDR=172.50.1.2
 
 # N times
@@ -18,7 +18,7 @@ for n in `seq $N_TIMES` ;
 do
 
     docker-compose exec -T bob /code/scripts/iperf3_wolfssl_start_client.sh \
-        $SERVER_IP_ADDR $DATA_SIZE $TOPIC "unlimited_"$(date +%s)
+        $SERVER_IP_ADDR $TEST_DURATION $TOPIC "unlimited_"$(date +%s)
 
 done
 
